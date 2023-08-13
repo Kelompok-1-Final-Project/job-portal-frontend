601573bb611cd311651620bc125d43fafb16feab
Torangto Situngkir <torangsitungkir93@gmail.com>
2023-08-13 21:28:58 +0700

    add quesiton

---
+++ projects/admin/src/app/pages/skill-test/question/question.routing.ts
@@ -0,0 +1,53 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { FileUploadModule } from 'primeng/fileupload';
+import { DialogModule } from 'primeng/dialog';
+import { QuestionListComponent } from "./list/question-list.component";
+import { QuestionInsertComponent } from "./insert/question-insert.component";
+import { SharedModuleComponent } from "@shared/shared.module";
+import { QuestionDetailComponent } from "./detail/question-detail.component";
+
+
+const routes : Routes = [
+    {
+        path : '',
+        component : QuestionListComponent
+    },
+    {
+        path : 'detail/:id',
+        component : QuestionDetailComponent
+    },
+    {
+        path : 'create',
+        component : QuestionInsertComponent
+    }
+];
+
+@NgModule({
+    declarations : [
+        QuestionListComponent,
+        QuestionInsertComponent,
+        QuestionDetailComponent
+    ],
+    imports : [
+        RouterModule.forChild(routes),
+        CommonModule,
+        ReactiveFormsModule,
+        ButtonModule,
+        DropdownModule,
+        SharedModuleComponent
+    ],
+    exports : [
+        RouterModule
+    ]
+ } )
+
+export class QuestionRouting {
+
+}
---
+++ projects/admin/src/app/pages/skill-test/test/test.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { BenefitRouting} from "./benefit.routing";
+
+
+@NgModule({
+    imports : [
+        BenefitRouting
+    ]
+})
+
+export class BenefitModule{
+
+}
