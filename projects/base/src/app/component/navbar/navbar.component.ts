3656c2103401b324ea43a34da33fbce264bd64e5
Torangto Situngkir <torangsitungkir93@gmail.com>
2023-08-16 17:59:12 +0700

    fix bug quill

--- projects/base/src/app/component/navbar/navbar.component.ts
+++ projects/base/src/app/component/navbar/navbar.component.ts
@@ -46,15 +46,10 @@ export class NavbarComponent implements OnInit {
 
   // contoh
   items: MenuItem[] | undefined;
+  itemsCandidate : MenuItem[] | undefined;
 
   ngOnInit() {
-    //
-    // const profile = this.authService.getProfile();
-    // if (profile) {
-    //   this.imgUrl = profile.photoId;
-    //   this.roleCode = profile.roleCode
-    // }
-    //Primeng
+
     this.items = [{
       label: 'Ticketing - Torangto',
       icon: 'pi pi-fw pi-ticket',
@@ -70,6 +65,185 @@ export class NavbarComponent implements OnInit {
       items: [{
         label: 'User',
         routerLink: "/users",
+        icon: 'pi pi-fw pi-user',
+        // visible : this.isAdmin
+      },
+      {
+        label: 'Company',
+        routerLink: "/companies",
+        icon: 'pi pi-fw pi-building',
+        // visible : this.isAdmin
+      },
+      {
+        label: 'Benefit',
+        routerLink: "/benefits",
+        icon: 'pi pi-fw pi-book',
+        // visible : this.isAdmin
+      },
+      {
+        label: 'Candidate',
+        routerLink: "/candidates",
+        icon: 'pi pi-fw pi-box',
+        // visible : this.isAdmin
+      },
+      {
+        label: 'City',
+        routerLink: "/cities",
+        icon: 'pi pi-fw pi-users',
+        // visible : this.isAdmin
+      },
+      {
+        label: 'Industry',
+        routerLink: "/industries",
+        icon: 'pi pi-fw pi-box',
+        // visible : this.isCustomer
+      },
+      {
+        label: 'Skill',
+        routerLink: "/skills",
+        icon: 'pi pi-fw pi-ticket',
+        // visible : this.isPic
+      },
+      {
+        label: 'Skill Test',
+        icon: 'pi pi-fw pi-ticket',
+        routerLink: "/skill-test",
+        // visible : this.isDeveloper
+      },
+      ]
+    },
+    {
+      label: 'Candidate Status',
+      icon: 'pi pi-fw pi-power-off',
+      items: [
+        {
+          label: 'Application',
+          routerLink: "/candidate-statuses/application",
+          icon: 'pi pi-fw pi-user',
+          // visible : this.isAdmin
+        },
+        {
+          label: 'Assessment',
+          routerLink: "/candidate-statuses/assessment",
+          icon: 'pi pi-fw pi-user',
+          // visible : this.isAdmin
+        },
+        {
+          label: 'Medical Checkup',
+          routerLink: "/candidate-statuses/mcu",
+          icon: 'pi pi-fw pi-user',
+          // visible : this.isAdmin
+        },
+        {
+          label: 'Interview',
+          routerLink: "/candidate-statuses/interview",
+          icon: 'pi pi-fw pi-user',
+          // visible : this.isAdmin
+        },
+        {
+          label: 'Offering',
+          routerLink: "/candidate-statuses/offering",
+          icon: 'pi pi-fw pi-user',
+          // visible : this.isAdmin
+        },
+        {
+          label: 'Hired',
+          routerLink: "/candidate-statuses/hired",
+          icon: 'pi pi-fw pi-user',
+          // visible : this.isAdmin
+        },
+        {
+          label: 'Blacklist',
+          icon: 'pi pi-fw pi-ticket',
+          routerLink: "/blacklist",
+          // visible : this.isDeveloper
+        }
+      ]
+    },
+    {
+      label: 'Question',
+      icon: 'pi pi-fw pi-ticket',
+      routerLink: "/questions",
+      // visible : this.isDeveloper
+    },
+    {
+      label: 'Job vacancy',
+      routerLink: "/job-vacancies",
+      icon: 'pi pi-fw pi-ticket',
+      // visible : this.isCustomer
+    },
+    {
+      label: 'Users',
+      icon: 'pi pi-fw pi-user',
+      items: [{
+        label: 'Profile',
+        icon: 'pi pi-fw pi-user',
+        routerLink: "/users/profile",
+      },
+      {
+        label: 'Change Password',
+        icon: 'pi pi-fw pi-cog',
+        routerLink: "/users/change-password",
+      },
+      {
+        label: 'Logout',
+        icon: 'pi pi-fw pi-power-off',
+        command: () => {
+          this.logout()
+        }
+      }
+      ]
+    },
+    {
+      label: 'About',
+      items: [{
+        label: 'Edit',
+        icon: 'pi pi-fw pi-pencil',
+        items: [{
+          label: 'Save',
+          icon: 'pi pi-fw pi-calendar-plus'
+        },
+        {
+          label: 'Delete',
+          icon: 'pi pi-fw pi-calendar-minus'
+        }
+        ]
+      },
+      {
+        label: 'Contact',
+        items: [{
+          label: 'Remove',
+          icon: 'pi pi-fw pi-calendar-minus'
+        }]
+      }
+      ]
+    },
+    {
+      label: 'Quit',
+      icon: 'pi pi-fw pi-power-off',
+      command: () => {
+        this.logout()
+      }
+    }
+    ];
+
+    // ========================================================
+  
+    this.itemsCandidate = [{
+      label: 'InLook - Home',
+      icon: 'pi pi-fw pi-ticket',
+      routerLink: "/home",
+    }, {
+      label: 'Home',
+      icon: 'pi pi-spin pi-box',
+      routerLink: "/home",
+    },
+    {
+      label: 'Master Data',
+      icon: "pi pi-fw pi-server",
+      items: [{
+        label: 'jobs',
+        routerLink: "/home/job",
         icon: 'pi pi-fw pi-user',
         // visible : this.isAdmin
       },
