213136215e0e81dda6fe942db0dd5b2af9ff30b7
firmanarrozi <ren.corp17@gmail.com>
2023-08-25 14:05:52 +0700

    frontend firman

--- projects/admin/src/app/pages/candidate-status/medical-checkup/list/mcu-list.component.ts
+++ projects/admin/src/app/pages/candidate-status/medical-checkup/list/mcu-list.component.ts
@@ -1,10 +1,9 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { MedicalCheckupGetResDto } from '@dto/medicalcheckup/medical-checkup.get.res.dto';
 import { StatusProgressService } from '@serviceAdmin/statusprogress.service';
+import { FileUpload } from 'primeng/fileupload';
 import { firstValueFrom } from 'rxjs';
 
 @Component({
@@ -13,24 +12,69 @@ import { firstValueFrom } from 'rxjs';
 })
 export class McuListComponent implements OnInit {
 
-  visibleUpdateStatus:boolean=false;
+  visibleUpdateStatus: boolean = false;
   mcus!: MedicalCheckupGetResDto[]
-  
+  visibleMcu: boolean = false
+
+  medicalUpdateReqDto = this.fb.group({
+    medicalId: ['', [Validators.required]],
+    file: ['', [Validators.required]],
+    ext: ['', [Validators.required]]
+  })
+
   constructor(
-    private title : Title,
-    private statusProgressService: StatusProgressService
-  ){
+    private title: Title,
+    private statusProgressService: StatusProgressService,
+    private fb: NonNullableFormBuilder
+  ) {
     this.title.setTitle('Medical Checkup | Job Portal Admin')
   }
-  
+
   ngOnInit(): void {
     this.getMedicalCheckup()
   }
 
-  getMedicalCheckup(){
+  getMedicalCheckup() {
     firstValueFrom(this.statusProgressService.getMedicalCheckup()).then(result => {
       this.mcus = result
     })
+  }
+
+  addFile(id: string){
+    this.visibleMcu = true
+    this.medicalUpdateReqDto.get('medicalId')?.setValue(id)
+  }
+
+  updateMcu() {
+    const data = this.medicalUpdateReqDto.getRawValue()
+    firstValueFrom(this.statusProgressService.updateMcuFile(data)).then(result =>{
+      this.visibleMcu = false
+    })
+  }
+
+  fileUpload(event: any, fileUpload: FileUpload) {
+    const toBase64 = (file: File) => new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        if (typeof reader.result === "string") resolve(reader.result)
+      };
+      reader.onerror = error => reject(error);
+    });
+
+    for (let file of event.files) {
+      toBase64(file).then(result => {
+        const resultBase64 = result.substring(result.indexOf(",") + 1, result.length)
+        const resultExtension = file.name.substring(file.name.indexOf(".") + 1, file.name.length)
+
+        this.medicalUpdateReqDto.patchValue({
+          file: resultBase64,
+          ext: resultExtension
+        })
+        fileUpload.clear();
+
+      })
+    }
   }
 
 }
